980a0cd57e89832ad88cf23880629cd09d88953a
Marcelo Iepsen <iepsen@gmail.com>
2020-03-25 20:50:30 +0100

    Add a draft implementation for computer support

---
+++ src/computer/index.ts
@@ -0,0 +1,7 @@
+import colors from './colors'
+import navigation from './navigation'
+import numbers from './numbers'
+
+export const exit = () => window.close()
+
+export default { colors, navigation, numbers, exit }
---
+++ src/computer/navigation.ts
@@ -0,0 +1,43 @@
+/**
+ * ArrowLeft key
+ * @constant
+ * @type {number}
+ */
+export const keyArrowLeft: number = 37
+
+/**
+ * ArrowUp key
+ * @constant
+ * @type {number}
+ */
+export const keyArrowUp: number = 38
+
+/**
+ * ArrowRight key
+ * @constant
+ * @type {number}
+ */
+export const keyArrowRight: number = 39
+
+/**
+ * ArrowDown key
+ * @constant
+ * @type {number}
+ */
+export const keyArrowDown: number = 40
+
+/**
+ * Enter key
+ * @constant
+ * @type {number}
+ */
+export const keyEnter: number = 13
+
+/**
+ * Back key
+ * @constant
+ * @type {number}
+ */
+export const keyBack: number = 27
+
+export default { keyArrowLeft, keyArrowUp, keyArrowRight, keyArrowDown }
